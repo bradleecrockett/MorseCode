88e44d3c58efac812d3990c810b5b86c818f76d7
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2019-11-05 11:32:51 -0800

    Update MorseCode.py

--- MorseCode.py
+++ MorseCode.py
@@ -11,22 +11,22 @@
 
 from MorseDictionary import morse_code
 
-# Create a function called morse_encoder(msg: str) that takes a single string parameter and converts it into the
+# Create a function called morse_encoder(str_msg) that takes a single string parameter and converts it into the
 # corresponding morse code message.  Note that the dictionary keys are all capital letters
 # Sample input        -->     Return Value
 # morse_encoder("A")        -->     ".-"
-# morse_encoder("PYTHON")   -->     ".--.-.---....----."
+# morse_encoder("PYTHON")   -->     ".--. -.-- - .... --- -."
 def morse_encoder(str_msg):
     # TODO: Write the code for the morse_encoder function.
 
     return ""
 
 
-# Create a function called morse_decoder(msg: str) that takes a single string parameter (in morse code) and converts it into the
+# Create a function called morse_decoder(morse_msg) that takes a single string parameter (in morse code) and converts it into the
 # corresponding string message.  The message can be returned in all caps.
 # Sample input                          -->     Return Value
 # morse_decoder(".-")                   -->     "A"
-# morse_decoder(".--.-.---....----.")   -->     "PYTHON"
+# morse_decoder(".--. -.-- - .... --- -.")   -->     "PYTHON"
 def morse_decoder(morse_msg):
     # TODO: Challenge - Write the code for the morse_decoder function.
 
@@ -36,10 +36,10 @@ def morse_decoder(morse_msg):
 def main():
     # TODO: Write at least 4 tests to verify that your morse_encoder function works.
     print("Morse Encoder Testing")
-    print("The message 'SOS' expected result: "...---...", "\n\tActual result: ", morse_encoder("SOS"))
+    print("The message 'SOS' expected result: '... --- ...'", "\n\tActual result: ", morse_encoder("SOS"))
     
     print("\n\n")
     print("Morse Decoder Testing")
-    print("The message '...---...' expected result: "SOS", "\n\tActual result: ", morse_encoder("...---..."))
+    print("The message '... --- ...' expected result: 'SOS'", "\n\tActual result: ", morse_decoder("... --- ..."))
 
 if __name__ == "__main__": main()
