f4e51d723d539820fcda390e9bb55761f7bab4d4
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2024-05-21 08:21:15 -0700

    Create dictionary_notes.py

---
+++ dictionary_notes.py
@@ -0,0 +1,15 @@
+# Name:
+# Date:
+# Period:
+
+'''
+Watch the video and follow along: https://youtu.be/wa1XcMSBWd
+You should be able to anser the following questions:
+1) What is a dictionary?
+2) How do you declare a dictionary?
+3) How do you access elements of a dictionary?
+4) Do dictionaries have an order?
+5) What is a key value pair?
+6) Name 3 useful methods that are associated with dictionaries.
+
+'''
